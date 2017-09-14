f2cd8f3c9e60c0434c646ecbff7ebfbefd600932
pennypangCODE <pennypang.t@gmail.com>
2017-09-14 15:34:56 +1000

    mapping script

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //JS and leaflet stuff
 mapboxgl.accessToken = 'pk.eyJ1IjoicGVubnlwYW5nY29kZSIsImEiOiJjajZ2bjlwazkxNW10MzJvMzVidjNlcnExIn0.kxfe2U4_vp8dcb6dQdok2g';
+
 var map = new mapboxgl.Map({
-    container: 'map', // container id
+  container: 'map', // container id
     style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
     center: [151.209900, -33.865143], // starting position [lng, lat]
     zoom: 10 // starting zoom
