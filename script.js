eaab7364825b8308e21c41b3c733effdbc199317
pennypangCODE <pennypang.t@gmail.com>
2017-09-14 10:05:28 +1000

    getting NSW map displayed

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 //JS and leaflet stuff
-
-var map = L.map('map'),{
-  center: [5,28],
-  zoom: 3,
-  minZoom:2,
-  maxZoom: 18
+mapboxgl.accessToken = 'pk.eyJ1IjoicGVubnlwYW5nY29kZSIsImEiOiJjajZ2bjlwazkxNW10MzJvMzVidjNlcnExIn0.kxfe2U4_vp8dcb6dQdok2g';
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+    center: [151.209900, -33.865143], // starting position [lng, lat]
+    zoom: 10 // starting zoom
 });
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}'),{
-  attribution:____________________
-  id:'mapbox.streets',
-  accessToken:'pk.eyJ1IjoicGVubnlwYW5nY29kZSIsImEiOiJjajZ2bjlwazkxNW10MzJvMzVidjNlcnExIn0.kxfe2U4_vp8dcb6dQdok2g'
-}).addTo(map);
-
-//Add some GeoJson
