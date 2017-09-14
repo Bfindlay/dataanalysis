72983d895d23d32140923dd78db3990bdd8d6037
bfindlay <bfin5330@uni.sydney.edu.au>
2017-09-14 18:48:28 +1000

    fixed map origin

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let map = L.map('map').setView([30.2744, 100.7751], 2.5);
+let map = L.map('map').setView([-33.8, 151.2], 6);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
